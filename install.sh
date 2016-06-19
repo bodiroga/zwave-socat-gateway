1d1836373255a7ce1f7b8427e2cc2c3ea21a9106
Aitor <riturrioz@gmail.com>
2016-06-19 14:32:47 +0200

    First working version of the install.sh file

--- install.sh
+++ install.sh
@@ -1,3 +1,40 @@
-// Instalar socat y git
+#!/bin/bash
+
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]; then
+   echo -e "This script must be run as root" 1>&2
+   exit 1
+fi
+
+echo -e '\nInstalling the required programs...'
 sudo apt-get --assume-yes install socat git
 
+cd /tmp
+
+echo -e '\nCloning the github repository...'
+git clone https://github.com/bodiroga/zwave-socat-gateway.git
+cd zwave-socat-gateway
+
+echo -e '\nMoving the udev rule to the corresponding folder...'
+cp -rf udev/* /etc/udev/rules.d/
+
+echo -e '\nMoving the program files to the /root directory...'
+cp -rf files/* /root
+
+cd /root
+if [ ! -f CONFIGURATION ]; then
+	echo -e '\nCreating the CONFIGURATION file, edit the parameters to meet your needs...'
+	cp CONFIGURATION_DEFAULT CONFIGURATION
+else
+	echo -e '\nYour CONFIGURATION file already exists, we will not touch it...'
+fi
+
+echo -e '\nCreating a crontab entry for the root user...'
+{ crontab -l -u root; echo '@reboot /usr/bin/python /root/raspberry_checker.py'; } | crontab -u root -
+
+echo -e '\nRemoving the tmp folder...'
+rm -rf /tmp/zwave-socat-gateway
+
+echo -e '\n----------------------------------------------------------------------------'
+echo -e 'Go to the /root folder, read the README file and edit the CONFIGURATION file'
+echo -e '----------------------------------------------------------------------------'
